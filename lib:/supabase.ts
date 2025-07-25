5f2c54f65e02a411ef0cff4fdd8b6ae1c43867b6
MyDira <aharonpanigel@gmail.com>
2025-07-24 22:39:03 -0400

    Add files via upload

--- lib:/supabase.ts
+++ lib:/supabase.ts
@@ -1,8 +1,6 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createClient } from '@supabase/supabase-js';
 
-export function createClient() {
-  return createBrowserClient(
-    'https://utgtbofebaompxhpcpvg.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0Z3Rib2ZlYmFvbXB4aHBjcHZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTQ4MDMsImV4cCI6MjA2ODg3MDgwM30.J_GzH_c6mrT-DPpkS2t-2EyKehubjwr5OG2Elxp36u8'
-  );
-}
+export const supabase = createClient(
+  'https://utgtbofebaompxhpcpvg.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0Z3Rib2ZlYmFvbXB4aHBjcHZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTQ4MDMsImV4cCI6MjA2ODg3MDgwM30.J_GzH_c6mrT-DPpkS2t-2EyKehubjwr5OG2Elxp36u8'
+);
