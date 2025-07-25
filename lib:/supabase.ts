96e7f44bd8673f215f0fe1a57f79bc95f383d2a8
MyDira <aharonpanigel@gmail.com>
2025-07-24 22:46:56 -0400

    Delete lib:/supabase.ts

--- lib:/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://utgtbofebaompxhpcpvg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0Z3Rib2ZlYmFvbXB4aHBjcHZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTQ4MDMsImV4cCI6MjA2ODg3MDgwM30.J_GzH_c6mrT-DPpkS2t-2EyKehubjwr5OG2Elxp36u8'
-);
